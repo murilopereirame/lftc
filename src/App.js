75ea7154882d35c6e0e5fdcab000d048ada75000
Murilo Pereira <mrex@tuta.io>
2021-01-05 15:07:18 -0300

    fix: Reinit

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
-import React from 'react';
+import logo from './logo.svg';
 import './App.css';
 
-const App = () => {
+function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src="" className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
